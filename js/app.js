e806f0fe8497d6d1f8e4dc95b6a15281fea35c32
Jeferson sena <jefinho368@gmail.com>
2023-10-02 08:16:50 -0300

    Manipulando Etapas do Carrinho (parte 5)

--- js/app.js
+++ js/app.js
@@ -126,16 +126,6 @@ cardapio.metodos = {
         $(".badge-total-carrinho").html(total);
     },
 
-
-    abrirCarrinho: (abrir) => {
-        if(abrir){
-            $("#modalCarrinho").removeClass('hidden');
-        }
-        else{
-            $("#modalCarrinho").addClass('hidden');
-        }
-    },
-
     mensagem: (texto, cor = 'red', tempo = 3500) =>{
 
         let id = Math.floor(Date.now() * Math.random().toString());
@@ -152,6 +142,77 @@ cardapio.metodos = {
             }, 800);
         }, tempo);
     },
+
+
+    abrirCarrinho: (abrir) => {
+        if(abrir){
+            $("#modalCarrinho").removeClass('hidden');
+            cardapio.metodos.carregarEtapa(1)
+        }
+        else{
+            $("#modalCarrinho").addClass('hidden');
+        }
+    },
+
+    // altera os textos e exibe os botões das etapas
+    carregarEtapa: (etapa) =>{
+        if(etapa == 1){
+            $("#lblTituloEtapa").text('Seu carrinho:');
+            $("#itensCarrinho").removeClass('hidden');
+            $("#localEntrega").addClass('hidden');
+            $("resumoCarrinho").addClass('hidden');
+
+            $(".etapa").removeClass('active');
+            $(".etapa-1").addClass('active');
+
+            $("#btnEtapaPedido").removeClass('hidden')
+            $("#btnEtapaEndereco").addClass('hidden')
+            $("#btnEtapaResumo").addClass('hidden')
+            $("#btnVoltar").addClass('hidden')
+
+        }
+        if(etapa == 2){
+
+            $("#lblTituloEtapa").text('Endereço de entrega:');
+            $("#itensCarrinho").addClass('hidden');
+            $("#localEntrega").removeClass('hidden');
+            $("resumoCarrinho").addClass('hidden');
+
+            $(".etapa").removeClass('active');
+            $(".etapa-1").addClass('active');
+            $(".etapa-2").addClass('active');
+
+            $("#btnEtapaPedido").addClass('hidden')
+            $("#btnEtapaEndereco").removeClass('hidden')
+            $("#btnEtapaResumo").addClass('hidden')
+            $("#btnVoltar").removeClass('hidden')
+
+        }
+        if(etapa == 3){
+
+            $("#lblTituloEtapa").text('Resumo do pedido:');
+            $("#itensCarrinho").addClass('hidden');
+            $("#localEntrega").addClass('hidden');
+            $("#resumoCarrinho").removeClass('hidden');
+
+            $(".etapa").removeClass('active');
+            $(".etapa-1").addClass('active');
+            $(".etapa-2").addClass('active');
+            $(".etapa-3").addClass('active');
+
+            $("#btnEtapaPedido").addClass('hidden')
+            $("#btnEtapaEndereco").addClass('hidden')
+            $("#btnEtapaResumo").removeClass('hidden')
+            $("#btnVoltar").removeClass('hidden')
+
+        }
+    },
+    
+    voltarEtapa: () => {
+
+        let etapa = $(".etapa.active").length;
+        cardapio.metodos.carregarEtapa(etapa - 1);
+    }
 }
 
 
